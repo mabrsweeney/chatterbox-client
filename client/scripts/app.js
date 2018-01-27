6905681a2762d8a0505e0173bc9e87251976b94f
mabrsweeney <mabrsweeney@gmail.com>
2018-01-27 12:32:49 -0800

    introduced rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,24 @@
 // YOUR CODE HERE:
 
-$(document).ready(function(){
+$(document).ready(function() {
   var app = {
     
-    server:'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    rooms: {},
+    friends: {},
 
     init: function() {
       app.fetch();
-      setInterval(function(){
+      setInterval(function() {
         app.fetch();
-      }, 2000);
+      }, 6000);
 
-      $('#send').on('submit', function(e){
+      $('#send').on('submit', function(e) {
         e.preventDefault();
         app.handleSubmit();
       });
+
+      $(document).on('change', '#roomSelect', app.fetch);
     },
 
     fetch: function() {
@@ -29,6 +33,12 @@ $(document).ready(function(){
         success: function({results}) {
           app.clearMessages();
           for (var i = 0; i < results.length; i++) {
+
+            //Add rooms
+            if (results[i].roomname !== undefined && app.rooms[results[i].roomname] === undefined) {
+              app.renderRoom(results[i].roomname);
+              app.rooms[results[i].roomname] = results[i].roomname;
+            }
             app.renderMessage(results[i]);
           }
         },
@@ -36,6 +46,7 @@ $(document).ready(function(){
           console.log('error!');
         }
       });
+      
     },
 
     send: function(value) {
@@ -58,7 +69,7 @@ $(document).ready(function(){
     },
 
     renderMessage: function({username, text, roomname}) {
-      var chat = $(`<div class='chat'></div>`);
+      var chat = $('<div class="chat"></div>');
       var usrnm = $('<a class=username></a>');
       var textElem = $('<p></p>');
       textElem.text(text);
@@ -73,7 +84,7 @@ $(document).ready(function(){
     }, 
 
     renderRoom: function(room) {
-      $('#roomSelect').append($(`<ul>${room}</ul>`));
+      $('#roomSelect').append($(`<option>${room}</option>`));
     },
 
     handleUsernameClick: function() {
@@ -81,18 +92,15 @@ $(document).ready(function(){
     },
 
     handleSubmit: function() {
-      var data = $("#send :input").serialize();
+      var data = $('#send :input').serialize();
       //console.log(data);
       var username = (/=.*/).exec(window.location.search)[0].slice(1);
       //console.log(username);
-      app.send({username:username, text:data});
+      app.send({username: username, text: data});
     }  
   };
 
   console.log('document ready');
   app.init();
-
-
-
   
 });
